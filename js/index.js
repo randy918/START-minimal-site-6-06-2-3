8acdd94208af4d5fb1e373c7d175dab24c78166b
Randy Miller <randy918@gmail.com>
2024-06-16 17:27:59 -0500

    6-16-24-1727 beginning

--- js/index.js
+++ js/index.js
@@ -1,29 +1,18 @@
+//?  ████████████████████████████████████  STRICT MODE
+
 'use strict'
 
-//^ ████████████████████████████████████  GLOBAL SCOPE
+//?  ████████████████████████████████████  IMPORTS
 
-//^ ████████████████████████████████████  GLOBAL SCOPE VARIABLES
+//^  ████████████████████████████████████  GLOBAL SCOPE
 
-const orders = [
-    { amount: 250 },
-    { amount: 450 },
-    { amount: 150 },
-    { amount: 350 },
-]
+//^  ████████████████████████████████████  GLOBAL SCOPE VARIABLES
 
 
-//_ ████████████████████████████████████  VARIABLE EXPRESSIONS
+//_  ████████████████████████████████████  VARIABLE EXPRESSIONS
 
-const totalAmount = orders.reduce(function (sum, order) {
-c("hello", sum, order);
-    return sum + order.amount
 
-}, 0)
 
 
 //>  ████████████████████████████████████  PROGRAM
 
-
-const numbers = [1, 2, 3, 4, 5];
-const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(totalAmount); // Output: 15
